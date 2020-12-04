a20c16bd8a84c5a44c7cecf4ce0ffbc74a0faad8
Julien Bronner <julbronner@free.fr>
2020-12-04 17:40:50 +0100

    ajout liste train

--- agence/views.py
+++ agence/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from .models import train
 
 def index(request):
-    template = loader.get_template('agence/index.html')
-    return HttpResponse(template.render(request=request))
+    # template = loader.get_template('agence/index.html')
+    context = {}
+    return render(request, 'agence/index.html', context)
+    # return HttpResponse(template.render(request=request))
+
+def listing(request):
+    trains = train.objects.all()
+    context = {
+        'trains': trains
+    }
+    return render(request, 'agence/all_train.html', context)
