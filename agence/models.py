288f93e0c89f893653f5c45fbde1c7e64ba87c51
Leo Bourdet <leobourdet@Leos-MacBook-Pro.local>
2020-12-04 13:45:06 +0100

    Ajout des relations v2

--- agence/models.py
+++ agence/models.py
@@ -1,39 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class voiture(models.Model):
-    numero_de_serie_voiture = models.IntegerField(null=False)
-    Num_train = models.ForeignKey(numero_train,on_delete=models.CASCADE) #ok
-	#numero_train = models.IntegerField(default=0)
-    nombre_place_dispo = models.IntegerField(null=False)
-
-class train(models.Model):
-    numero_train = models.IntegerField(null=False)
-    depart_ville = models.CharField(max_length=200,null=False)
-    arrivée_ville = models.CharField(max_length=200,null=False)
-
-class reservation(models.Model):
-	numero_reservation = models.IntegerField(null=False)
-	Numero_billet = models.ForeignKey(numero_billet,on_delete=models.CASCADE) #ok
-	#numero_billet = models.IntegerField(null=False)
-	Num_client = models.ForeignKey(numero_client,on_delete=models.CASCADE) #ok
-	#numero_client = models.IntegerField(null=False)
-	Num_place = models.ForeignKey(numero_place,on_delete=models.CASCADE) #PAS SUR, ONE TO ONE ??
-	#numero_place = models.IntegerField(null=False)
-	numero_voiture = models.IntegerField(null=False)
-	confirmation = models.BooleanField(null=False)
-	prix_total=models.DecimalField(max_digits=6, decimal_places=2,null=False)
-
 class reduction(models.Model):
-    numero_reduction = models.IntegerField(null=False)
-    type_reduction = models.CharField(max_length=200,null=False)
-    pourcentage = models.IntegerField(null=False)
-
-class place(models.Model):
-    Num_serie_voit = models.ForeignKey(numero_de_serie_voiture,on_delete=models.CASCADE) #ok
-	#numero_de_serie_voiture = models.IntegerField(null=False)
-    numero_place = models.IntegerField(null=False)
-    position = models.CharField(max_length=200,null=False)
+	numero_reduction = models.IntegerField(null=False)
+	type_reduction = models.CharField(max_length=200,null=False)
+	pourcentage = models.IntegerField(null=False)
 
 class client(models.Model):
     numero_client= models.IntegerField(null=False)
@@ -41,12 +12,29 @@ class client(models.Model):
     prenom = models.CharField(max_length=200,null=False)
     confirmation = models.BooleanField(null=False)
     majorite = models.BooleanField(null=False)
-    Num_reduc = models.ForeignKey(numero_reduction,on_delete=models.CASCADE) #ok
-	#numero_reduction = models.IntegerField(default=0)
+    numero_reduction = models.ForeignKey(reduction,on_delete=models.CASCADE) #ok
+	#numero_reduction = models.IntegerField(default=0)	
+
+class train(models.Model):
+	numero_train = models.IntegerField(null=False)
+	depart_ville = models.CharField(max_length=200,null=False)
+	arrivée_ville = models.CharField(max_length=200,null=False)
+
+class voiture(models.Model):
+	numero_de_serie_voiture = models.IntegerField(null=False)
+	numero_train = models.ForeignKey(train,on_delete=models.CASCADE) #ok
+	#numero_train = models.IntegerField(default=0)
+	nombre_place_dispo = models.IntegerField(null=False)
+
+class place(models.Model):
+	numero_de_serie_voiture = models.ForeignKey(voiture,on_delete=models.CASCADE) #ok
+	#numero_de_serie_voiture = models.IntegerField(null=False)
+	numero_place = models.IntegerField(null=False)
+	position = models.CharField(max_length=200,null=False)
 
 class billet(models.Model):
 	numero_billet = models.IntegerField(null=False)
-	Numero_train = models.ForeignKey(numero_train,on_delete=models.CASCADE) #ok
+	numero_train = models.ForeignKey(train,on_delete=models.CASCADE) #ok
 	#numero_train = models.IntegerField(null=False)
 	agence = models.CharField(max_length=200,null=False)
 	prix_billet=models.DecimalField(max_digits=6, decimal_places=2,null=False)
@@ -56,6 +44,19 @@ class billet(models.Model):
 	arrivee_ville = models.CharField(max_length=200,null=False)
 	arrivee_date = models.DateTimeField(null=False)
 	arrivee_heure = models.TimeField(null=False)
+
+class reservation(models.Model):
+	numero_reservation = models.IntegerField(null=False)
+	numero_billet = models.ForeignKey(billet,on_delete=models.CASCADE) #ok
+	#numero_billet = models.IntegerField(null=False)
+	numero_client = models.ForeignKey(client,on_delete=models.CASCADE) #ok
+	#numero_client = models.IntegerField(null=False)
+	numero_place = models.ForeignKey(place,on_delete=models.CASCADE) #PAS SUR, ONE TO ONE ??
+	#numero_place = models.IntegerField(null=False)
+	numero_voiture = models.IntegerField(null=False)
+	confirmation = models.BooleanField(null=False)
+	prix_total=models.DecimalField(max_digits=6, decimal_places=2,null=False)
+
 
 
 
